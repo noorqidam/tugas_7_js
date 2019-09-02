96a294762549501ad5c591b32d1d0461765fabe1
noorqidam <noorqidam@gmail.com>
2019-09-02 15:16:56 +0700

    Tugas 7 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+// Menggunakan For
+var tinggibadan = [160,165,170,155,169,163,172,173,175,180];
+for(var i=0; i<tinggibadan.length; i++){
+  console.log(tinggibadan[i])
+}
+
+// Menggunakan For OF (tindak menggunakan index)
+for(let i of tinggibadan){
+  console.log(i);
+}
